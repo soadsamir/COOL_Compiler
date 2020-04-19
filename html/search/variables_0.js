2f4c592d9202a389d7efc89ae8e4ded0117f88e9
AmiraGamal9 <61991044+AmiraGamal9@users.noreply.github.com>
2020-04-20 00:21:34 +0200

    Delete variables_0.js

--- html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tokennames',['tokenNames',['../class_c_o_o_l_lexer.html#aeca3daaa0800a5d42b4e46929d03717d',1,'COOLLexer.tokenNames()'],['../class_c_o_o_l_parser.html#a2ff4286ec4012efaa86eee8a6e6ce82a',1,'COOLParser.tokenNames()']]]
-];
