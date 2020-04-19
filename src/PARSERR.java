f078f8fd7b368507505f9945d0715df84988db59
Nadine Abdallah <48635698+nadineabdallah@users.noreply.github.com>
2020-04-19 19:04:27 +0200

    Update PARSERR.java

--- src/PARSERR.java
+++ src/PARSERR.java
@@ -6,7 +6,16 @@ import org.antlr.v4.runtime.tree.ParseTree;
         import java.io.IOException;
 
 @SuppressWarnings("ALL")
-
+/**
+ *
+ *  This is a <b>Parser </b> class that interprets some functionalities for the parser {@link PARSER}
+ *  as a component of the compiler, that transforms a string of tokens into a syntax tree.
+ *
+ *  <p>Parser produces a parse tree
+ *     based on the rules declared in the grammar.
+ *    The lexer rules specify the tokens whereas the parser rules specify the tree.</p>
+ *
+ */
 
 public class PARSERR {
 
@@ -14,12 +23,14 @@ public class PARSERR {
     private ParseTree cachedParseTree;
 
     /**
-     * The purpose of this constructor is to intialize an object <b>coolParser</b> of the
-     * class <b> PARSER </b> , with a stream of tokens get from the lexer.
+     * The purpose of this constructor is to intialize an object <b>coolParser</b>
+     * that takes a pre-defined stream of tokens emitted by the lexical analyzer , as an input.
+     * Parsing is done at the token level,
+     * When a token trigger an error , it will be tracked.
      *
      * @param lexer
      * The lexer is an input parameter to the constructor,
-     * and when a token trigger an error , it will be tracked.
+     *
      */
     public PARSERR(Lexer lexer) {
         coolParser = new COOLParser(lexer.getTokensStream());
@@ -28,14 +39,15 @@ public class PARSERR {
     }
 
     /**
-     * This function named <b>getCST</b> is used to convert a pre-defined stream of tokens
-     * into a context syntax tree and output it in string format.
+     *
+     * This function named <b>getCST</b>
+     * is used to construct a context syntax tree from the parse tree.
      *
      * @param
      * no input parameters.
-     *
      * @return
      * a string , representing a syntax tree.
+     *
      */
     public String getCST() {
         COOLParser.ProgramContext program = coolParser.program();
@@ -50,9 +62,9 @@ public class PARSERR {
      *
      * @param fileName
      * is the input file in which the context syntax tree is printed.
-     *
      * @return
      * {@link void} , no return type for this function.
+     *
      */
 
     public void writeCST(String fileName){
@@ -68,11 +80,13 @@ public class PARSERR {
     }
 
     /**
+     *
      * It is an interface to access the tree ,
      * that has no input parameters.
      *
      * @return
      * This returns the tree created during the parse.
+     *
      */
     public ParseTree parse() {
         // coolParser.addParseListener(new CoolBaseListener());
