906a9486f1934d0e7b2a5d1962c4f1f870030b70
AmiraGamal9 <61991044+AmiraGamal9@users.noreply.github.com>
2020-04-20 00:24:44 +0200

    Delete pages_0.js

--- html/search/pages_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['cool_20compiler',['COOL Compiler',['../md_out_production__compiler__r_e_a_d_m_e.html',1,'']]],
-  ['cool_20compiler',['COOL Compiler',['../md__r_e_a_d_m_e.html',1,'']]]
-];
